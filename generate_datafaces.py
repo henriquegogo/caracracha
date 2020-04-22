507d01becf60e9990b1edf9061636da18612deb6
Henrique Gogó <henriquegogo@gmail.com>
2020-04-22 02:31:20 -0300

    Fixing some lint issues

--- generate_datafaces.py
+++ generate_datafaces.py
@@ -1,16 +1,17 @@
+""" Generate processed faces database """
 from sys import argv
 from glob import glob
 import face_recognition
 
-files = glob(argv[1])
-datafaces = {}
+FILES = glob(argv[1])
+DATAFACES = {}
 
-for filepath in files:
+for filepath in FILES:
     name = filepath.split(".")[0].split("/")[-1]
     image = face_recognition.load_image_file(filepath)
     image_encodings = face_recognition.face_encodings(image)
     if image_encodings:
-        datafaces[name] = list(image_encodings[0])
+        DATAFACES[name] = list(image_encodings[0])
 
 print("known_faces = ")
-print(datafaces)
+print(DATAFACES)
--- main.py
+++ main.py
@@ -1,19 +1,22 @@
+""" Web application that identifies images from a pre processed database """
 import face_recognition
 import numpy as np
-from flask import Flask, jsonify, request, redirect
+from flask import Flask, request
 from datafaces import known_faces
 
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
-app = Flask(__name__)
+APP = Flask(__name__)
 
 def allowed_file(filename):
+    """ Verifies if file extension is compatible """
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-@app.route('/')
+@APP.route('/')
 def home():
+    """ Home interface """
     return '''<!doctype html>
 <meta name="viewport" content="width=device-width, initial-scale=1" />
 <body style="margin:0;font-family:sans-serif;color:white">
@@ -28,24 +31,25 @@ def home():
 </body>
 '''
 
-@app.route('/analyse', methods=['POST'])
+@APP.route('/analyse', methods=['POST'])
 def upload():
+    """ Route that receive file and start process """
     file = request.files['file']
-
-    if file and allowed_file(file.filename):
-        return detect_faces_in_image(file)
-
+    return detect_faces_in_image(file) if file and allowed_file(file.filename) else None
 
 def detect_faces_in_image(file_stream):
+    """ Detect faces in image """
     img = face_recognition.load_image_file(file_stream)
     unknown_face_encodings = face_recognition.face_encodings(img)
 
     face_name = "Nenhum rosto detectado"
 
     if len(unknown_face_encodings) > 0:
-        distances = face_recognition.face_distance(list(known_faces.values()), unknown_face_encodings[0])
+        distances = face_recognition.face_distance(list(known_faces.values()),
+                                                   unknown_face_encodings[0])
         match_index = np.argsort(distances)[0]
-        face_name = list(known_faces.keys())[match_index] if distances[match_index] <= 0.6 else "Pessoa desconhecida"
+        face_name = list(known_faces.keys())[match_index] \
+            if distances[match_index] <= 0.6 else "Pessoa desconhecida"
 
     return '''
 <body style="margin:0;padding:20px;height:100%;font-family:sans-serif;text-align:center;font-size:3em;background-image:url(https://blog.doity.com.br/wp-content/uploads/2019/01/crach%C3%A1-para-evento.png);background-size:150%;background-repeat:no-repeat;background-position:center top">
@@ -54,4 +58,4 @@ def detect_faces_in_image(file_stream):
 '''
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    APP.run(host='0.0.0.0', port=5001, debug=True)
